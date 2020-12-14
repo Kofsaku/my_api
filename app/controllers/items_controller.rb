59049a68d508a1e4810983430009d04032f651ef
Kofsaku <51185002+Kofsaku@users.noreply.github.com>
2020-12-14 19:15:20 +0900

    indexにアクセスするとitemsのjsonが返ってくる

--- app/controllers/items_controller.rb
+++ app/controllers/items_controller.rb
@@ -1,8 +1,17 @@
 class ItemsController < ApplicationController
      def index          
-          render  :json => { title: "英語を効率的に学ぶための10の秘訣", body: "Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua. Ut enim ad minim veniam, quis nostrud exercitation ullamco laboris nisi ut aliquip ex ea commodo consequat. Duis aute irure dolor in reprehenderit in voluptate velit esse cillum dolore eu fugiat nulla pariatur. Excepteur sint occaecat cupidatat non proident, sunt in culpa qui officia deserunt mollit anim id est laborum." }
+          @items = Item.all
+          #items2 = Item.create(:title => "英語を使えるようになる1万の秘密", :body => "what you need to do first is to remember more than 100,000 words in a day.")
+         render  :json =>  @items
+
      end
 end
-#itemというモデルを作る。モデルのデータを複数個作ってみて。その複数を配列のjsonとして、返す。
-#↓
-#API経由で不要なデータをCRUDできるようにする。
+
+#別のdefを作る。
+#↑とは別にidごとにjsonを返すアクションの設定 @items = Item.find(params[:id])
+#特定の情報をとってくる
+#新しく作る
+#あるデータを削除するAPI
+#cd ../
+#ls 
+# 
--- config/routes.rb
+++ config/routes.rb
@@ -2,3 +2,6 @@ Rails.application.routes.draw do
   # For details on the DSL available within this file, see https://guides.rubyonrails.org/routing.html
   get "items" => "items#index"
 end
+ 
+#idごとのデータを返すようにする。index/id1みたいな
+#  get "items/:id" => "items#index"
