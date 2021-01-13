614008f463c328ee6a971aa1d3f53418452804ef
Kofsaku <51185002+Kofsaku@users.noreply.github.com>
2021-01-13 16:36:52 +0900

    item_searhcer_testファイル内の不要な記述を削除しました。

--- app/models/item_searcher_test.rb
+++ app/models/item_searcher_test.rb
@@ -1,15 +1,12 @@
-params = ActionController::Parameters.new({author: "織田信長" })
-
 params = ActionController::Parameters.new(author: "津端", keyword: "英語", tag_names: "")
-item_searcher = ItemSearcher.new(params, Item.all)
-item_searcher.execute =end
+item_searcher1 = ItemSearcher.new(params, Item.all)
 item_searcher2 = ItemSearcher.new(params, Item.all,"モテテクニック")
 item_searcher3 = ItemSearcher.new(params, Item.all, "織田", "", ["歴史","英語"])
 item_searcher4 = ItemSearcher.new(params, Item.all, ["歴史", "ミステリー"])
 item_searcher5 = ItemSearcher.new(params, Item.all, "", "プログラム", "")
 item_searcher6 = ItemSearcher.new(params, Item.all, "", "信長", "歴史")
-item_searcher7 = ItemSearcher.new(params, Item.all, "", "", "")
 item_searcher7 = ItemSearcher.new(params, Item.all, "津端", "英語", "")
+
 if item_searcher1.execute.count != 3
      raise "アウト"
 end
