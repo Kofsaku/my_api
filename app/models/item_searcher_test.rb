16f2890859586321877b40f8e50b53994fb91d86
Kofsaku <51185002+Kofsaku@users.noreply.github.com>
2021-01-13 17:28:27 +0900

    item_searcher_testファイルで動作確認を行えるようにしました。

--- app/models/item_searcher_test.rb
+++ app/models/item_searcher_test.rb
@@ -1,33 +1,53 @@
-params = ActionController::Parameters.new(author: "津端", keyword: "英語", tag_names: "")
-item_searcher1 = ItemSearcher.new(params, Item.all)
-item_searcher2 = ItemSearcher.new(params, Item.all,"モテテクニック")
-item_searcher3 = ItemSearcher.new(params, Item.all, "織田", "", ["歴史","英語"])
-item_searcher4 = ItemSearcher.new(params, Item.all, ["歴史", "ミステリー"])
-item_searcher5 = ItemSearcher.new(params, Item.all, "", "プログラム", "")
-item_searcher6 = ItemSearcher.new(params, Item.all, "", "信長", "歴史")
-item_searcher7 = ItemSearcher.new(params, Item.all, "津端", "英語", "")
-
-if item_searcher1.execute.count != 3
-     raise "アウト"
-end
-if item_searcher2.execute.count != 3
-     raise "アウト"
-end
-if item_searcher3.execute.count != 1
-     raise "アウト"
-end
-if item_searcher4.execute.count != 3
-     raise "アウト"
-end
-if item_searcher5.execute.count != 1 
-     raise "アウト"
-end
-if item_searcher6.execute.count != 2
-     raise "アウト"
-end
-if item_searcher7.execute.count != 2
+params = ActionController::Parameters.new(author: "", keyword: "", tag_names: ["英語","歴史"])
+item_searcher = ItemSearcher.new(params, Item.all)
+if item_searcher.execute.count != 1
      raise "アウト"
 end
 
+params = ActionController::Parameters.new(author: "", keyword: "プログラム", tag_names: "")
+item_searcher = ItemSearcher.new(params, Item.all)
+if item_searcher.execute.count != 1
+     raise "アウト"
+end
 
+params = ActionController::Parameters.new(author: "津端", keyword: "", tag_names: "")
+item_searcher = ItemSearcher.new(params, Item.all)
+if item_searcher.execute.count != 3
+     raise "アウト"
+end
 
+params = ActionController::Parameters.new(author: "", keyword: "", tag_names: "")
+item_searcher = ItemSearcher.new(params, Item.all)
+if item_searcher.execute.count != 44
+     raise "アウト"
+end
+
+params = ActionController::Parameters.new(author: "織田", keyword: "幼少期", tag_names: "ミステリー")
+item_searcher = ItemSearcher.new(params, Item.all)
+if item_searcher.execute.count != 1
+     raise "アウト"
+end
+
+params = ActionController::Parameters.new(author: "津端", keyword: "モテモテ", tag_names: ["モテテクニック", "英語"])
+item_searcher = ItemSearcher.new(params, Item.all)
+if item_searcher.execute.count != 1
+     raise "アウト"
+end
+
+params = ActionController::Parameters.new(author: "", keyword: "戦国大名", tag_names: ["ミステリー","歴史"])
+item_searcher = ItemSearcher.new(params, Item.all)
+if item_searcher.execute.count != 1
+     raise "アウト"
+end
+
+params = ActionController::Parameters.new(author: "織田", keyword: "", tag_names: "ミステリー")
+item_searcher = ItemSearcher.new(params, Item.all)
+if item_searcher.execute.count != 2
+     raise "アウト"
+end
+
+params = ActionController::Parameters.new(author: "織田", keyword: "本質", tag_names: "")
+item_searcher = ItemSearcher.new(params, Item.all)
+if item_searcher.execute.count != 1
+     raise "アウト"
+end
